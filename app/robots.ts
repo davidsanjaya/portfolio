51dc3aa99a669cc8b229fe49c3d54b83bbe143b3
DAVE\davis <dsanjaya@uxbee.nl>
2026-08-03 16:50:32 +0700

    add host in robot.text

--- app/robots.ts
+++ app/robots.ts
@@ -7,5 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
     },
     sitemap: "https://davidsanjaya.vercel.app/sitemap.xml",
+    host: "https://davidsanjaya.vercel.app",
   };
 }
